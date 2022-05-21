5c08d4435e631783524146372143672ef1e2672e
DreiK-one <Dreikone1@mail.ru>
2022-05-21 14:10:02 +0200

    Add film service and inteface

---
+++ MyFilms/MyFilms.Core/DTOs/FilmDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFilms.Core.DTOs
+{
+    public class FilmDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public float? Rating { get; set; }
+        public int CreationYear { get; set; }
+    }
+}
---
+++ MyFilms/MyFilms.Core/Interfaces/IFilmService.cs
@@ -0,0 +1,18 @@
+﻿using MyFilms.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFilms.Core.Interfaces
+{
+    public interface IFilmService
+    {
+        Task<FilmDto> AddFilm(FilmDto film);
+        Task<FilmDto> GetFilmById(Guid id);
+        Task<IEnumerable<FilmDto>> GetAllFilms();
+        Task<FilmDto> EditFilm(FilmDto film);
+        Task<int> DeleteFilm(Guid id);  
+    }
+}
